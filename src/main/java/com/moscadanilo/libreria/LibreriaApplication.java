699f1fe7be4bdc1b0ed785a71c96135c7d4b9a42
Danilo Mosca <danilo.mosca85@gmail.com>
2026-06-10 21:32:28 +0200

    Added initial controller and Genre enum

--- src/main/java/com/moscadanilo/libreria/LibreriaApplication.java
+++ src/main/java/com/moscadanilo/libreria/LibreriaApplication.java
@@ -3,11 +3,11 @@ package com.moscadanilo.libreria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication		// ← annotazione "tutto in uno"
 public class LibreriaApplication {
 
 	public static void main(String[] args) {
+		// Avvia il contesto Spring e il server embedded
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/moscadanilo/libreria/model/Genre.java
@@ -0,0 +1,11 @@
+package com.moscadanilo.libreria.model;
+
+public enum Genre {
+    TECH,
+    FICTION,
+    HISTORY,
+    ROMANCE,
+    THRILLER,
+    FANTASY,
+    BIOGRAPHY
+}
