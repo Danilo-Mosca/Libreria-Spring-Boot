d91d9e59ee5283fc1efc6013352acb861ce9b963
Danilo Mosca <danilo.mosca85@gmail.com>
2026-06-14 22:46:12 +0200

    added MySQL dependencies, validator, bootstrap, dotenv. Changed application.properties file and create .env and .env.example files

--- src/main/java/com/moscadanilo/libreria/controller/UserRestController.java
+++ src/main/java/com/moscadanilo/libreria/controller/UserRestController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/users")   // prefisso comune per tutti gli endpoint di questa classe
 public class UserRestController {
     
 }
