5ae8c3f3e65051bd10d5bdfc1829d7ffdaf6e923
Danilo Mosca <danilo.mosca85@gmail.com>
2026-08-05 21:28:13 +0200

    New borrowings entity created

---
+++ src/main/java/com/moscadanilo/libreria/model/Borrowing.java
@@ -0,0 +1,11 @@
+package com.moscadanilo.libreria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+// Creo una nuova identità (tabella) borrowings (prestiti)
+@Entity
+@Table(name = "borrowings")
+public class Borrowing {
+
+}
