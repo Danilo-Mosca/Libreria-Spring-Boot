019885e56486961445f305b351d3b5731bed08b2
Danilo Mosca <danilo.mosca85@gmail.com>
2026-08-05 21:49:34 +0200

    Added instance variables for the columns of the borrowings table

--- src/main/java/com/moscadanilo/libreria/model/Borrowing.java
+++ src/main/java/com/moscadanilo/libreria/model/Borrowing.java
@@ -1,11 +1,37 @@
 package com.moscadanilo.libreria.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 // Creo una nuova identità (tabella) borrowings (prestiti)
 @Entity
 @Table(name = "borrowings")
 public class Borrowing {
 
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Libro da cui dipendo
+
+    // Data di inizio prestito
+    @NotNull(message = "La data di inizio prestito non può essere nulla")
+    @PastOrPresent(message = "La data inserita per l'inizio del prestito non può essere una data futura")   // Regola che dice che la data in cui io vado a prendere in prestito il libro non può essere una data diversa da oggi o una data precedente a quella di oggi. Non permette che l'utente inserisca una data successiva a quella di oggi
+    private LocalDate borrowinDate;
+
+    // Data di ritorno del libro dato in prestito
+    @PastOrPresent(message = "La data inserita per il ritorno del libro preso in prestito non può essere una data futura")  // Come per borrowingDate
+    private LocalDate returnDate;
+
+    // Stringa di tipo Lob ovvero long object (oggetto di grandi dimensioni)
+    @Lob
+    private String notes;
 }
